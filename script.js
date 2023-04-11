1f0cb788332a08e38df72cfcc01746684aeb031f
Sardor <sardoribnfarhod@gmail.com>
2023-04-11 17:19:37 +0500

    top link

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const toggleBar = document.querySelector(".toggle-bar");
+// Elements
+const toggle = document.querySelector(".toggle-bar");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".nav-links");
+const header = document.querySelector(".header");
+const topLink = document.querySelector(".top-link");
 
 // Toggle menu
-toggleBar.addEventListener("click", () => {
+toggle.addEventListener("click", () => {
+  console.log("clicked");
   const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
   if (containerHeight === 0) {
@@ -13,6 +17,27 @@ toggleBar.addEventListener("click", () => {
   }
 });
 
+// Fixed header
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const headerHeight = header.getBoundingClientRect().height;
+
+  if (scrollHeight > headerHeight) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
+  }
+});
+
+// Top Link btn
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 // Dynamic footer year
 const yearEl = document.querySelector("#year");
 yearEl.innerHTML = new Date().getFullYear();
